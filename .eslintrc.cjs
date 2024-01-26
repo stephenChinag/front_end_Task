e5bac65d624069c67f3c0f9d17adb769064d6076
StpehenChinag <stephenchinag@gmail.com>
2024-01-26 17:12:48 +0100

    fixed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
+/* eslint-env node */
+
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -7,7 +8,6 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
@@ -16,6 +16,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'react/prop-types': 'off',
   },
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
-import WelcomePage from "./pages/Welcome.jsx";
-import ChallengesPage from "./pages/Challenges.jsx";
-
-import Authentication from "./pages/Authentication.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <WelcomePage />,
-    errorElement: <p> Page not found</p>,
-  },
-
-  { path: "/challenges", element: <ChallengesPage /> },
-
-  { path: "/auth", element: <Authentication /> },
-]);
-
-function App() {
-  return <RouterProvider router={router} />;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
+  );
+};
 
 export default App;
